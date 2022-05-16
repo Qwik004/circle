2291bba02c24b22bb77ade97faca8a10825503f6
Qwik005 <105530592+Qwik005@users.noreply.github.com>
2022-05-16 08:40:56 +0530

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,4 +1,4 @@
-Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -AsPlainText "gurusingh" -Force) ;
+Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -AsPlainText gurusingh -Force) ;
 Get-LocalUser -Name "Administrator" | Enable-LocalUser ;
 Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip ;
 Expand-Archive ngrok.zip . ;
